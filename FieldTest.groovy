2e1d79476602dd0be9c1cfd84ce024903ac306bf
Mann-Wahrenberg Peter Alexander (Mann) <alexander.mann-wahrenberg@continental-corporation.com>
2018-11-17 14:43:07 +0100

    Iteration 5. Begin

---
+++ FieldTest.groovy
@@ -0,0 +1,4 @@
+#!/usr/bin/env groovy
+
+// The class Game shall exist
+Field field = new Field()
---
+++ GameTest.groovy
@@ -0,0 +1,7 @@
+#!/usr/bin/env groovy
+
+// The class Game shall exist
+Game game = new Game()
+
+// My test
+assert 1 == 1
